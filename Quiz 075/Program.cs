d0c4dc48a93c2c8567f49dfece788707149663eb
Kan210346 <111563811+Kan210346@users.noreply.github.com>
2022-09-16 16:25:34 +0700

    Add files via upload

---
+++ Quiz 075/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+public class Program {
+    public static void Main(string[] args){
+        string input;
+        Console.WriteLine("Please Input a,b,c ");
+        input = Console.ReadLine();
+        
+        if(input == "a"){
+            Console.WriteLine("Name : Chulalongkorn University");
+                Console.WriteLine("Name Library : Rakdi");
+                Console.WriteLine("Adress : Khet PathumWan");
+                Console.WriteLine("call : 02-215-3555");
+                Console.WriteLine("---------------------------------------");
+                
+                
+                Console.WriteLine("Name : Mahidol University ");
+                Console.WriteLine("Name Library : Rak Lok");
+                Console.WriteLine("Adress : Khet Sala Ya");
+                Console.WriteLine("call : 02-849-6211");
+                Console.WriteLine("---------------------------------------");
+
+                Console.WriteLine("Name :  KMITL ");
+                Console.WriteLine("Name Library : Ke Ki");
+                Console.WriteLine("Adress : Lat Krabang");
+                Console.WriteLine("call : 02-329-8000");
+                Console.WriteLine("---------------------------------------");
+
+
+
+                Console.WriteLine("Name : Khon Kaen University ");
+                Console.WriteLine("Name Library : Watthana");
+                Console.WriteLine("Adress : Khet Nai MueangKhon Kean");
+                Console.WriteLine("Call : 043-002539");
+                Console.WriteLine("---------------------------------------");
+               
+
+                
+                Console.WriteLine("Name : KMUTT ");
+                Console.WriteLine("Name Library : A Thon");
+                Console.WriteLine("Adress : Khet ThungKharu");
+                Console.WriteLine("Call : 043-002539");
+                Console.WriteLine("---------------------------------------");
+                
+         }
+             else if (input == "b"){
+                Console.WriteLine("Name : Chulalongkorn University");
+                Console.WriteLine("Name : ChaiDi");
+                Console.WriteLine("Surname : RakDi");
+                Console.WriteLine("ID card : 153-698-4561237");
+                Console.WriteLine("---------------------------------------");
+
+                Console.WriteLine("Name : Mahidol University");
+                Console.WriteLine("Name : Non Di");
+                Console.WriteLine("Surname : Chaidi");
+                Console.WriteLine("ID card : 742-112-5687412");
+                Console.WriteLine("---------------------------------------");
+
+                Console.WriteLine("Name : KMITL");
+                Console.WriteLine("Name : Aphai Di");
+                Console.WriteLine("Surname : Rian Di");
+                Console.WriteLine("ID card : 541-772-3215689");
+                Console.WriteLine("---------------------------------------");
+
+                Console.WriteLine("Name : Khon Kaen University");
+                Console.WriteLine("Name : Tham Di");
+                Console.WriteLine("Surname : NganDi");
+                Console.WriteLine("ID card : 777-772-4578923");
+                Console.WriteLine("---------------------------------------");
+
+                Console.WriteLine("Name : KMUTT");
+                Console.WriteLine("Name : Non Di");
+                Console.WriteLine("Surname : Ria Bodi");
+                Console.WriteLine("ID card : 003-852-4752000");
+                Console.WriteLine("---------------------------------------");
+
+    
+            }
+            else if (input == "c"){
+            double[] FT = new double[5]{79.6,70.7,249,30.2,208.9};
+            double[] electricity_bill = new double[5]{7911,7725,25172,4369,21470};
+            
+            Console.WriteLine("Name University : KMUTT");
+                Console.WriteLine("Name Library : ");
+                Console.WriteLine("FT 12 month : ");
+                Console.WriteLine("electricity bill 12 month : ");
+                Console.WriteLine("---------------------------------------");
+
+
+
+
+            
+        
+        }     
+    }
+}
+
+
+
+            
+
+
+
+
+                
+                    
+                
+        
+        
+
+        
+
+        
+
+
+
+
+    
+
+
+
